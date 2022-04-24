8bd023e4edc0ad1fb58f068128b34050dbc92551
SANGHYUN KIM <haan823@naver.com>
2022-04-24 11:38:55 +0900

    strategy pattern sample

---
+++ src/designpattern/strategypattern/Strategy.java
@@ -0,0 +1,5 @@
+package designpattern.strategypattern;
+
+public interface Strategy {
+    void runStrategy();
+}
---
+++ src/designpattern/strategypattern/StrategySword.java
@@ -0,0 +1,8 @@
+package designpattern.strategypattern;
+
+public class StrategySword implements Strategy {
+    @Override
+    public void runStrategy() {
+        System.out.println("챙.. 채재챙 챙챙");
+    }
+}
